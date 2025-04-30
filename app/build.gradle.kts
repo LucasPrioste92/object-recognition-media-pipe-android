8c7e1c858833172c4b882008f6c974d2d8dc5a4c
LucasPrioste <lucasprioste@hotmail.com>
2025-04-30 16:09:19 +0100

    add mediapipe and cameraX dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,20 +40,30 @@ android {
 }
 
 dependencies {
-
+    // UI
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // CameraX
+    implementation(libs.bundles.camerax)
+
+    // MediaPipe
+    implementation(libs.mediapipe.tasks.vision)
 }
